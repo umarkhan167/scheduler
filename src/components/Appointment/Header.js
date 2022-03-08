fd6cc3e82c12a10effa081aecd18e40e6a67466b
umarkhan167 <77021740+umarkhan167@users.noreply.github.com>
2022-03-08 15:03:48 -0500

    Add all story components to application

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Header = (props) => {
-  // const { time } = props
   return (
 <header className="appointment__time">
   <h4 className="text--semi-bold">{props.time}</h4>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+// import Header from "components/Appointment/Header";
+// import Show from "components/Appointment/Show";
+// import Empty from "components/Appointment/Empty";
 
 const Appointment = (props) => {
   return (
